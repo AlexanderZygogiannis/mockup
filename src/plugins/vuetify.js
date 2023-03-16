d0e6e4d11d9e9085e1171f426e9fb3f9b2cd0e2f
Alexandros Zygogiannis <azygogiannis@25-8projects.com>
2023-03-16 13:41:36 +0200

    added fonts icons

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,9 @@
-import Vue from 'vue';
-import Vuetify from 'vuetify/lib/framework';
+import "material-design-icons-iconfont/dist/material-design-icons.css"; // Ensure you are using css-loader
+import Vue from "vue";
+import Vuetify from "vuetify/lib/framework";
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
+  iconfont: "md",
 });
